ec242f133089f9d526190e1c18eb4cdf0408e82d
gardenkim <syzygy@g.yju.ac.kr>
2024-01-24 15:48:47 +0900

    Design: Create a base_appbar

---
+++ lib/widgets/base_appbar.dart
@@ -0,0 +1,59 @@
+import "package:flutter/material.dart";
+
+class BaseAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const BaseAppBar({
+    Key? key,
+    this.title,
+  }) : super(key: key);
+
+  final String? title;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget titleWidget;
+
+    // title 매개변수 전달 여부에 따라 위젯을 다르게 생성
+    if (title != null) {
+      titleWidget = Text(
+        title!,
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w200,
+          fontSize: 20,
+        ),
+      );
+    } else {
+      titleWidget = const Center(
+        child: Image(
+          image: AssetImage('assets/img/yju_logo.png'),
+          width: 150,
+        ),
+      );
+    }
+
+    return AppBar(
+      centerTitle: true,
+      leading: const Icon(
+        Icons.menu,
+        color: Colors.white,
+        size: 30,
+      ),
+      actions: [
+        // 알림창
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.notifications,
+            color: Colors.white,
+            size: 30,
+          ),
+        ),
+      ],
+      // 앱바 타이틀
+      title: titleWidget,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(50);
+}
