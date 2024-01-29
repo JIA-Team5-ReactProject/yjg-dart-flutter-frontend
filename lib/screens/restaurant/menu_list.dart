1f2dfecaa5af94d5f368631304ab8b0ad9d6c665
LMH <ahdehfdl333@naver.com>
2024-01-29 15:20:44 +0900

    Feat : メニューリトの変更

--- lib/screens/restaurant/menu_list.dart
+++ lib/screens/restaurant/menu_list.dart
@@ -19,7 +19,9 @@ DateTime _lastDayOfMonth(DateTime date) {
 }
 
 class _MenuListState extends State<MenuList> {
-  DateTime? _selectedDay; // 사용자가 선택한 날짜
+  List<String>? _selectedMenu;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
 
   @override
   Widget build(BuildContext context) {
@@ -35,97 +37,155 @@ class _MenuListState extends State<MenuList> {
             decoration: BoxDecoration(
               border: Border.all(color: Color.fromARGB(255, 241, 240, 240)),
               borderRadius: BorderRadius.circular(15.0),
-    //           boxShadow: [
-    //   BoxShadow(
-    //     color: Colors.grey.withOpacity(0.5), // 그림자의 색상 설정
-    //     spreadRadius: 5, // 그림자가 퍼지는 정도 설정
-    //     blurRadius: 7, // 그림자의 흐림 정도 설정
-    //     offset: Offset(0, 3), // 그림자의 위치 조정
-    //   ),
-    // ],
             ),
 
             child: TableCalendar(
+              locale: 'ko-KR', //달력 언어 한국어로
               firstDay: _firstDayOfMonth(DateTime.now()), // 캘린더의 첫번째 날짜
               lastDay: _lastDayOfMonth(DateTime.now()), // 캘린더의 마지막 날짜
               focusedDay: DateTime.now(), // 초기 포커스 되는 날짜
-              //보이는 형식을 월만 허용하게 해서 보이는 형식 바꾸는 버튼 삭제
-              availableCalendarFormats: const {
-                CalendarFormat.month: 'Month'
-              }, // 캘린더 형식
+
+              // 선택된 날짜를 결정하는 설정 추가
               selectedDayPredicate: (day) {
-                // 선택된 날짜를 결정하는 함수
-                return isSameDay(_selectedDay, day); // 선택된 날짜와 같은 날인지 확인
+                return isSameDay(_selectedDay, day);
               },
-              onDaySelected: (selectedDay, focusedDay) {
-                // 날짜가 선택되었을 때의 콜백 함수
-                if (!isSameDay(_selectedDay, selectedDay)) {
-                  // 선택된 날짜가 업데이트 되었을 때만 setState 호출
-                  setState(
-                    () {
-                      _selectedDay = selectedDay;
-                    },
-                  );
-                }
-              },
+
+              //보이는 형식을 월만 허용하게 해서 보이는 형식 바꾸는 버튼 삭제
+              availableCalendarFormats: const {CalendarFormat.month: 'Month'},
+
               headerStyle: const HeaderStyle(
-                  leftChevronVisible: false, // 왼쪽 화살표 버튼 숨깁.
-                  rightChevronVisible: false, // 오른쪽 화살표 버튼 숨깁.
-                  headerMargin: EdgeInsets.only(left: 20)),
+                leftChevronVisible: false, // 왼쪽 화살표 버튼 숨깁.
+                rightChevronVisible: false, // 오른쪽 화살표 버튼 숨깁.
+                headerMargin: EdgeInsets.only(left: 20),
+              ),
+
+              //날짜가 클릭 됐을 때 실행되는 것
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _focusedDay = focusedDay;
+                  _selectedDay = selectedDay;
+                  _selectedMenu = [
+                    breakfastMenu[_selectedDay!.day % breakfastMenu.length],
+                    lunchMenu[_selectedDay!.day % lunchMenu.length],
+                    dinnerMenu[_selectedDay!.day % dinnerMenu.length]
+                  ];
+                });
+              },
             ),
           ),
-          if (_selectedDay != null)
-            // 선택된 날짜가 있을 때만 메뉴 카드를 보여줌
-            menuCard(_selectedDay!),
+          menuCard()
         ],
       ),
     );
   }
 
-  //날짜 클릭 시 조,중,석 메뉴 보여주는 위젯카드
-  Widget menuCard(DateTime date) {
-    // 선택한 날짜를 yyyy-mm-dd 형식의 문자열로 변환
-    String formattedDate = DateFormat('yyyy-MM-dd').format(date);
+//음식 메뉴 리스트로 담아놓기 (하드코딩)
+  List<String> breakfastMenu = ['안성탕면', '김치', '콩밥'];
+  List<String> lunchMenu = ['신라면', '파김치', '현미밥'];
+  List<String> dinnerMenu = ['진라면(순)', '총각김치', '치자밥'];
 
-    return Container(
-      width: MediaQuery.of(context).size.width, // 메뉴 카드 너비 설정
-      margin: EdgeInsets.all(7),
-      
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Align(
-            alignment: Alignment.topRight,
-            child: IconButton(
-              icon: const Icon(Icons.close),
-              onPressed: () {
-                setState(() {
-                  _selectedDay =
-                      null; // 'x' 버튼을 누르면 선택된 날짜를 null로 설정하여 메뉴 카드를 숨김
-                });
-              },
+//날짜 클릭 이벤트 위젯
+  Widget menuCard() {
+    if (_selectedDay == null) {
+      return SizedBox.shrink(); // _selectedDay가 null이면 아무것도 표시하지 않음
+    } else {
+      return Material(
+          elevation: 5.0, 
+          child: Container(
+            width: 380,
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: Color.fromARGB(255, 139, 139, 139),
+                width: 0.5,
+              ),
             ),
-          ),
-          Text(formattedDate,
-            style:
-              const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
-
-          const SizedBox(height: 16.0),
-          const Text('조식',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
-          const Text('안성탕면, 김치, 콩밥'),
-
-          const SizedBox(height: 8.0),
-          const Text('중식',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
-          const Text('신라면, 파김치, 현미밥'),
-
-          const SizedBox(height: 8.0),
-          const Text('석식',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
-          const Text('진라면(순), 총각김치, 치자밥'),
-        ],
-      ),
-    );
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                    margin: EdgeInsets.only(left: 25, top: 10),
+                    child: Text(
+                      DateFormat('yyyy-MM-dd')
+                          .format(_selectedDay!), // 선택된 날짜를 yyyy-MM-dd 형식으로 표시
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center, // 가로방향으로 중앙에 정렬
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(20),
+                      child: Column(
+                        crossAxisAlignment:
+                            CrossAxisAlignment.center, // 세로방향으로 중앙에 정렬
+                        children: [
+                          const Text(
+                            "조식",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 29, 127, 159),
+                                fontWeight: FontWeight.bold,fontSize: 20),
+                                
+                          ),
+                          ...breakfastMenu
+                              .map((menu) => Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(menu),
+                                  ))
+                              .toList(),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(20),
+                      child: Column(
+                        crossAxisAlignment:
+                            CrossAxisAlignment.center, // 세로방향으로 중앙에 정렬
+                        children: [
+                          const Text(
+                            "중식",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 29, 127, 159),
+                                fontWeight: FontWeight.bold,fontSize: 20),
+                          ),
+                          ...lunchMenu
+                              .map((menu) => Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(menu),
+                                  ))
+                              .toList(),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(20),
+                      child: Column(
+                        crossAxisAlignment:
+                            CrossAxisAlignment.center, // 세로방향으로 중앙에 정렬
+                        children: [
+                          const Text(
+                            "석식",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 29, 127, 159),
+                                fontWeight: FontWeight.bold,fontSize: 20),
+                          ),
+                          ...dinnerMenu
+                              .map((menu) => Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(menu),
+                                  ))
+                              .toList(),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ));
+    }
   }
 }
