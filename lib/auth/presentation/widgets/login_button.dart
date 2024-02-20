8b4f903bc6139ef5c5b9b88f55a5b2d1eca49cb0
gardenkim <syzygy@g.yju.ac.kr>
2024-02-20 16:38:21 +0900

    Feat: ログイン処理ロジックの追加

--- lib/auth/presentation/widgets/login_button.dart
+++ lib/auth/presentation/widgets/login_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:yjg/auth/presentation/viewmodels/user_viewmodel.dart';
+import 'package:yjg/auth/domain/usecases/login_usecase.dart';
 import 'package:yjg/shared/theme/palette.dart';
 
 class LoginButton extends ConsumerWidget {
@@ -16,9 +16,28 @@ class LoginButton extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return ElevatedButton(
-      onPressed: () => ref
-          .read(authProvider.notifier)
-          .login(emailController.text, passwordController.text, ref, context),
+      onPressed: () async {
+        if (emailController.text.isNotEmpty && passwordController.text.isNotEmpty) {
+          // Form이 유효한 경우에만 회원가입 로직 실행
+          final loginUseCase =
+              LoginUseCase(ref: ref); // RegisterUseCase 인스턴스 생성
+
+          // execute 메소드를 비동기적으로 호출하고, 사용자 입력을 전달
+          await loginUseCase.execute(
+            email: emailController.text,
+            password: passwordController.text,
+            context: context,
+          );
+        } else {
+          // Form이 유효하지 않은 경우, 사용자에게 알림
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('입력되지 않은 필드가 있습니다. 다시 한 번 확인해 주세요.'),
+              backgroundColor: Palette.mainColor,
+            ),
+          );
+        }
+      },
       style: ButtonStyle(
         backgroundColor: MaterialStateProperty.resolveWith<Color>((states) {
           if (states.contains(MaterialState.pressed)) {
