52c3d5a7cf8be01e00908b17a5f248259b4692d7
gardenkim <syzygy@g.yju.ac.kr>
2024-02-27 17:05:19 +0900

    Feat: Drawerにログアウトロジック追加

--- lib/shared/widgets/base_drawer.dart
+++ lib/shared/widgets/base_drawer.dart
@@ -1,9 +1,46 @@
 import "package:dotted_line/dotted_line.dart";
 import "package:flutter/material.dart";
 import 'package:yjg/shared/theme/palette.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart'; // flutter_secure_storage 패키지
 
-class BaseDrawer extends StatelessWidget {
-  const BaseDrawer({super.key});
+class BaseDrawer extends StatefulWidget {
+  const BaseDrawer({Key? key}) : super(key: key);
+
+  @override
+  State<BaseDrawer> createState() => _BaseDrawerState();
+}
+
+class _BaseDrawerState extends State<BaseDrawer> {
+  static final storage = FlutterSecureStorage();
+  dynamic userInfo = '';
+
+  // 로그아웃
+
+  logout() async {
+    await storage.delete(key: 'auth_token');
+    debugPrint('로그아웃 완료');
+    Navigator.pushNamed(context, '/login_domestic');
+  }
+
+  checkUserState() async {
+    userInfo = await storage.read(key: 'auth_token');
+    if (userInfo == null) {
+      debugPrint('토큰 미존재. 로그인 페이지로 이동');
+      Navigator.pushNamed(context, '/login_domestic'); // 로그인 페이지로 이동
+    } else {
+      debugPrint('로그인 중');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 비동기로 flutter secure storage 정보를 불러오는 작업
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      checkUserState();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -161,6 +198,30 @@ class BaseDrawer extends StatelessWidget {
             title: const Text('회의실 예약'),
             trailing: const Icon(Icons.navigate_next),
             onTap: () {},
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+            child: DottedLine(
+              dashColor: Palette.stateColor4,
+              dashLength: 2,
+              lineLength: double.infinity, // 길이 조절
+              lineThickness: 2, // 두께 조절
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout_outlined),
+            iconColor: Palette.stateColor3,
+            focusColor: Palette.stateColor3,
+            title: const Text(
+              '로그아웃',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            onTap: () {
+              logout();
+            },
           ),
           SizedBox(
             height: 40,
