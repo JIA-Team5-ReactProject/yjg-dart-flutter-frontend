0a9dc91d852bbe1cabe56912530ec5db4b6f2e82
gardenkim <syzygy@g.yju.ac.kr>
2024-02-28 01:11:27 +0900

    Refactor: トークン交換ロジックの修正

--- lib/auth/data/data_resources/google_login_data_source.dart
+++ lib/auth/data/data_resources/google_login_data_source.dart
@@ -4,7 +4,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
+import 'package:yjg/auth/data/models/token_response.dart';
 import 'package:yjg/auth/presentation/viewmodels/user_viewmodel.dart';
+import 'package:yjg/main.dart';
 import 'package:yjg/shared/constants/api_url.dart';
 
 class GoogleLoginDataSource {
@@ -68,24 +70,37 @@ class GoogleLoginDataSource {
         "postGoogleLoginAPI 토큰 교환 결과: ${response.body}, ${response.statusCode}");
 
     if (response.statusCode == 200) {
-      // final jsonDate = json.decode(response.body);
-      // Tokengenerated result = Tokengenerated.fromJson(jsonDate);
-      // int? id = result.data?.user?.id;
+      final jsonData = json.decode(response.body);
+      Tokengenerated result = Tokengenerated.fromJson(jsonData);
 
-      // if (id != null) {
-      //   // id 값을 StateNotifier를 통해 업데이트
-      //   ref.read(userIdProvider.notifier).setUserId(id);
-      // }
-      // String? token = result.data?.token;
+      int? id = result.user?.id; // 응답으로부터 사용자 ID 추출
+      int? approved = result.user?.approved;
+      if (id != null) {
+        // 새 사용자 ID로 상태 업데이트
+        ref.read(userIdProvider.notifier).setUserId(id);
+      } else {
+        // ID가 null인 경우 처리
+        debugPrint('사용자 ID가 null입니다.');
+      }
 
-      // if (token != null) {
-      //   await storage.write(key: 'auth_token', value: token);
-      // } else {
-      //   // throw Exception('토큰이 없습니다.');
-      //   debugPrint('토큰이 없습니다.');
-      // }
+      String? token = result.token; // 응답으로부터 토큰 추출
+
+      if (token != null) {
+        // 토큰을 저장하기 위해 사용
+        await storage.write(key: 'auth_token', value: token);
+        if (approved == 1) {
+          // 토큰이 null이 아니고, 승인된 계정인 경우
+          debugPrint('승인된 계정, 토큰: $token');
+          navigatorKey.currentState!.pushNamed('/dashboard_main'); // 메인으로 이동
+        } else {
+          navigatorKey.currentState!
+              .pushNamed('/registration_detail'); // 추가 정보 입력 페이지로 이동
+        }
+      } else {
+        // 토큰이 null인 경우 처리
+        debugPrint('토큰이 없습니다.');
+      }
       return response;
-      // return result;
     } else {
       throw Exception('로그인 실패: ${response.statusCode}');
     }
