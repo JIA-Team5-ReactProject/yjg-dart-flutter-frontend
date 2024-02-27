7a0a66530867f386861a4beae1171e15f8a1adc3
gardenkim <syzygy@g.yju.ac.kr>
2024-02-27 15:39:40 +0900

    Feat: ユーザーエンティティにテキストフォーム更新メソッド追加

--- lib/auth/domain/entities/user.dart
+++ lib/auth/domain/entities/user.dart
@@ -5,14 +5,18 @@ class User extends ChangeNotifier {
   String password;
   String name;
   String phoneNumber;
+  String displayName;
+  String studentId;
 
-  User({
-    required this.email,
-    required this.password,
-    required this.name,
-    required this.phoneNumber,
-  });
+  User(
+      {required this.email,
+      required this.password,
+      required this.name,
+      required this.phoneNumber,
+      required this.displayName,
+      required this.studentId,});
 
+  // 로그인 폼 업데이트
   void loginFormUpdate({
     String? email,
     String? password,
@@ -20,20 +24,47 @@ class User extends ChangeNotifier {
     if (email != null) this.email = email;
     if (password != null) this.password = password;
 
-    notifyListeners();
+    notifyListeners(); // User 객체가 변경되었음을 알림
   }
-  
+
+  // 회원가입 폼 업데이트
   void registerFormUpdate({
     String? email,
     String? password,
     String? name,
     String? phoneNumber,
+    String? studentId,
   }) {
     if (email != null) this.email = email;
     if (password != null) this.password = password;
     if (name != null) this.name = name;
     if (phoneNumber != null) this.phoneNumber = phoneNumber;
+    if (studentId != null) this.studentId = studentId;
+ 
+    notifyListeners(); // User 객체가 변경되었음을 알림
+  }
 
-    notifyListeners();
+  // 추가 정보 입력 폼 업데이트
+  void additionalInfoFormUpdate({
+    String? studentId,
+    String? name,
+    String? phoneNumber,
+  }) {
+    if (studentId != null) this.studentId = studentId;
+    if (name != null) this.name = name;
+    if (phoneNumber != null) this.phoneNumber = phoneNumber;
+
+    notifyListeners(); // User 객체가 변경되었음을 알림
+  }
+
+  // 구글 로그인 폼 업데이트
+  void updateWithGoogleSignIn({
+    String? email,
+    String? displayName,
+  }) {
+    if (email != null) this.email = email;
+    if (displayName != null) this.displayName = displayName;
+
+    notifyListeners(); // User 객체가 변경되었음을 알림
   }
 }
