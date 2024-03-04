26a8e14440ab6a7487e3c48f3128965499ed83cd
gardenkim <syzygy@g.yju.ac.kr>
2024-03-05 01:04:50 +0900

    Feat: 美容室の価格表フィルターボタンUIおよび選択後の状態管理ロジックの実装

---
+++ lib/salon/presentaion/widgets/filter_group_botton.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:group_button/group_button.dart';
+import 'package:yjg/salon/presentaion/viewmodels/category_viewmodel.dart';
+import 'package:yjg/salon/presentaion/viewmodels/user_selection_viewmodel.dart';
+
+import 'package:yjg/shared/theme/palette.dart';
+
+class FilterGroupButton extends ConsumerWidget {
+  FilterGroupButton({Key? key, required this.dataType}) : super(key: key);
+  final String dataType;
+  List<String> genderData = ['male', 'female'];
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // 비동기 데이터 로드
+    final categoryDataAsyncValue = ref.watch(categoryListProvider);
+    final userSelection = ref.watch(userSelectionProvider);
+
+    // AsyncValue: 데이터가 로드중인지, 성공적으로 로드됐는지, 오류 발생했는지 등 다양한 상태를 나타내는 객체
+    // .when(): AsyncValue의 상태에 따라 다른 위젯을 표시(data, loading, error)
+    return categoryDataAsyncValue.when(
+      data: (Map<int, String> categoryData) {
+        // 데이터 로드 성공 시, UI 구성
+        final categoryNames = categoryData.values.toList();
+        final buttons = dataType == '성별' ? genderData : categoryNames;
+
+        return GroupButton(
+          isRadio: true,
+          buttons: buttons,
+          onSelected: (val, i, selected) {
+            if (dataType == '성별') {
+              // 성별 선택을 상태로 저장
+              ref.read(userSelectionProvider.notifier).setSelectedGender(val);
+            } else {
+              // 카테고리 선택의 경우, val이 카테고리 이름이므로, 이를 id로 변환해야 함(카테고리 이름으로 카테고리 ID 찾기)
+              int? categoryId = categoryData.keys
+                  .firstWhere((k) => categoryData[k] == val, orElse: () => -1);
+              if (categoryId != null) {
+                // 유효한 카테고리 ID가 있다면 상태 업데이트
+                ref
+                    .read(userSelectionProvider.notifier)
+                    .setSelectedCategoryId(categoryId);
+              }
+              debugPrint(ref.read(userSelectionProvider).toString());
+            }
+          },
+          controller: GroupButtonController(
+            selectedIndex: dataType == '성별'
+                ? buttons.indexOf(userSelection.selectedGender)
+                : categoryData.keys
+                    .toList()
+                    .indexOf(userSelection.selectedCategoryId),
+          ),
+          options: GroupButtonOptions(
+            buttonWidth: 90.0,
+            buttonHeight: 40.0,
+            unselectedColor: Colors.grey[300],
+            unselectedTextStyle: TextStyle(
+              color: Colors.grey[600],
+            ),
+            selectedColor: Palette.mainColor.withOpacity(0.8),
+            selectedBorderColor: Palette.mainColor,
+            selectedTextStyle: TextStyle(
+              color: Colors.white,
+            ),
+            borderRadius: BorderRadius.circular(30),
+          ),
+        );
+      },
+      loading: () => SizedBox(
+        width: 20,
+        height: 20,
+        child: const CircularProgressIndicator(color: Palette.stateColor4),
+      ),
+      error: (e, stack) => Text('Error: $e'),
+    );
+  }
+}
