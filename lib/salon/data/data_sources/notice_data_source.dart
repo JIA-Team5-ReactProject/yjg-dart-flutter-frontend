95ecacd008d37b7628cd7b6c8f9e5107b7317c61
gardenkim <syzygy@g.yju.ac.kr>
2024-03-13 15:25:06 +0900

    Chore: プリント削除

--- lib/salon/data/data_sources/notice_data_source.dart
+++ lib/salon/data/data_sources/notice_data_source.dart
@@ -24,8 +24,6 @@ class NoticeDataSource {
 
     Uri uri = Uri.parse(url).replace(queryParameters: queryParams);
 
-    debugPrint(uri.toString());
-
     final response = await http.get(
       uri,
       headers: <String, String>{
