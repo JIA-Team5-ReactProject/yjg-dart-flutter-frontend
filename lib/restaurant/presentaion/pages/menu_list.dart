a756158847cbced8fd8cb7fee576b04854cd791b
LMH <ahdehfdl333@naver.com>
2024-03-22 10:04:26 +0900

    Fix: アスペクト比エラーを修正

--- lib/restaurant/presentaion/pages/menu_list.dart
+++ lib/restaurant/presentaion/pages/menu_list.dart
@@ -33,22 +33,31 @@ class _MenuListState extends State<MenuList> {
 
   //해당 날짜의 식단표를 불러오는 API 함수
   Future<void> fetchMenus(DateTime selectedDay) async {
-    final String formattedDate = DateFormat('yyyy-MM-dd').format(selectedDay);
-    final String url = '$apiURL/api/restaurant/menu/get/d?date=$formattedDate';
+  final String formattedDate = DateFormat('yyyy-MM-dd').format(selectedDay);
+  final String url = '$apiURL/api/restaurant/menu/get/d?date=$formattedDate';
 
-    String? token = await storage.read(key: 'auth_token');
+  String? token = await storage.read(key: 'auth_token');
 
-    try {
-      final response = await http.get(
-        Uri.parse(url),
-        headers: {
-          'Authorization': 'Bearer $token',
-          'Content-Type': 'application/json',
-        },
-      );
+  try {
+    final response = await http.get(
+      Uri.parse(url),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+    );
 
-      if (response.statusCode == 200) {
-        final data = json.decode(response.body)['month_menus'];
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body)['month_menus'];
+
+      if (data.isEmpty) {
+        // API에서 가져온 리스트가 비어있을 경우 기본 메시지를 설정
+        setState(() {
+          breakfastMenu = ['입력 된', '메뉴가', '없습니다'];
+          lunchMenu = ['입력 된', '메뉴가', '없습니다'];
+          dinnerMenu = ['입력 된', '메뉴가', '없습니다'];
+        });
+      } else {
         List<String> newBreakfastMenu = [];
         List<String> newLunchMenu = [];
         List<String> newDinnerMenu = [];
@@ -78,13 +87,14 @@ class _MenuListState extends State<MenuList> {
           lunchMenu = newLunchMenu;
           dinnerMenu = newDinnerMenu;
         });
-      } else {
-        throw Exception('Failed to load menus');
       }
-    } catch (e) {
-      print(e.toString());
+    } else {
+      throw Exception('Failed to load menus');
     }
+  } catch (e) {
+    print(e.toString());
   }
+}
 
   @override
   Widget build(BuildContext context) {
@@ -92,50 +102,52 @@ class _MenuListState extends State<MenuList> {
       appBar: const BaseAppBar(title: '식단표'),
       drawer: const BaseDrawer(),
       bottomNavigationBar: const CustomBottomNavigationBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            //컨테이너 디자인(달력)
-            margin: EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              border: Border.all(color: Color.fromARGB(255, 241, 240, 240)),
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-
-            child: TableCalendar(
-              locale: 'ko-KR', //달력 언어 한국어로
-              firstDay: _firstDayOfMonth(DateTime.now()), // 캘린더의 첫번째 날짜
-              lastDay: _lastDayOfMonth(DateTime.now()), // 캘린더의 마지막 날짜
-              focusedDay: DateTime.now(), // 초기 포커스 되는 날짜
-
-              // 선택된 날짜를 결정하는 설정 추가
-              selectedDayPredicate: (day) {
-                return isSameDay(_selectedDay, day);
-              },
-
-              //보이는 형식을 월만 허용하게 해서 보이는 형식 바꾸는 버튼 삭제
-              availableCalendarFormats: const {CalendarFormat.month: 'Month'},
-
-              headerStyle: const HeaderStyle(
-                leftChevronVisible: false, // 왼쪽 화살표 버튼 숨깁.
-                rightChevronVisible: false, // 오른쪽 화살표 버튼 숨깁.
-                headerMargin: EdgeInsets.only(left: 20),
+      body: SingleChildScrollView(
+        // 스크롤 가능한 구조로 변경
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              //컨테이너 디자인(달력)
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                border: Border.all(color: Color.fromARGB(255, 241, 240, 240)),
+                borderRadius: BorderRadius.circular(15.0),
               ),
+              child: TableCalendar(
+                locale: 'ko-KR', //달력 언어 한국어로
+                firstDay: _firstDayOfMonth(DateTime.now()), // 캘린더의 첫번째 날짜
+                lastDay: _lastDayOfMonth(DateTime.now()), // 캘린더의 마지막 날짜
+                focusedDay: DateTime.now(), // 초기 포커스 되는 날짜
 
-              //날짜가 클릭 됐을 때 실행되는 것
-              onDaySelected: (selectedDay, focusedDay) {
-                setState(() {
-                  _focusedDay = focusedDay;
-                  _selectedDay = selectedDay;
-                  fetchMenus(selectedDay); // API 호출 함수 실행
-                });
-              },
+                // 선택된 날짜를 결정하는 설정 추가
+                selectedDayPredicate: (day) {
+                  return isSameDay(_selectedDay, day);
+                },
+
+                //보이는 형식을 월만 허용하게 해서 보이는 형식 바꾸는 버튼 삭제
+                availableCalendarFormats: const {CalendarFormat.month: 'Month'},
+
+                headerStyle: const HeaderStyle(
+                  leftChevronVisible: false, // 왼쪽 화살표 버튼 숨김
+                  rightChevronVisible: false, // 오른쪽 화살표 버튼 숨김
+                  headerMargin: EdgeInsets.only(left: 20),
+                ),
+
+                //날짜가 클릭 됐을 때 실행되는 것
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    _focusedDay = focusedDay;
+                    _selectedDay = selectedDay;
+                    fetchMenus(selectedDay); // API 호출 함수 실행
+                  });
+                },
+              ),
             ),
-          ),
-          
-          menuCardContainer(),
-        ],
+
+            menuCardContainer(), // 메뉴 카드를 표시하는 컨테이너
+          ],
+        ),
       ),
     );
   }
@@ -145,25 +157,25 @@ class _MenuListState extends State<MenuList> {
   List<String> lunchMenu = [];
   List<String> dinnerMenu = [];
 
-Widget menuCardContainer() {
-  return Container(
-    margin: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-    decoration: BoxDecoration(
-      border: Border.all(color: Colors.grey),
-      borderRadius: BorderRadius.circular(15.0),
-    ),
-    child: SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: [
-          SizedBox(width: 16), // 시작 여백
-          menuCard(),
-          SizedBox(width: 16), // 끝 여백
-        ],
+  Widget menuCardContainer() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(15.0),
       ),
-    ),
-  );
-}
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            SizedBox(width: 16), // 시작 여백
+            menuCard(),
+            SizedBox(width: 16), // 끝 여백
+          ],
+        ),
+      ),
+    );
+  }
 
 //날짜 클릭 이벤트 위젯
   Widget menuCard() {
