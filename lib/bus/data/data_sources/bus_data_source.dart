52cfb4e7e5655423f92b9102b0006672cf3e88d2
gardenkim <syzygy@g.yju.ac.kr>
2024-03-29 11:50:26 +0900

    Refactor: Bus APIにDioインターセプター導入

--- lib/bus/data/data_sources/bus_data_source.dart
+++ lib/bus/data/data_sources/bus_data_source.dart
@@ -1,19 +1,19 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:http/http.dart' as http;
 import 'package:yjg/shared/constants/api_url.dart';
+import 'package:yjg/shared/service/interceptor.dart';
 
 class BusDataSource {
-  String getApiUrl() {
-    // 상수 파일에서 가져온 apiURL 사용
-    return apiURL;
-  }
-
+  static final Dio dio = Dio();
   static final storage = FlutterSecureStorage(); // 토큰 담는 곳
 
+  BusDataSource() {
+    dio.interceptors.add(DioInterceptor(dio));
+  }
+
 // * 버스 데이터
-  Future<http.Response> getBusDataAPI(int weekend, int semester, String route) async {
-    final token = await storage.read(key: 'auth_token');
+  Future<Response> getBusDataAPI(int weekend, int semester, String route) async {
      String url = '$apiURL/api/bus/round/appSchedule';
 
     debugPrint('weekend: $weekend, semester: $semester, route: $route');
@@ -24,22 +24,12 @@ class BusDataSource {
       'bus_route_direction': route,
     }; 
 
-    Uri uri = Uri.parse(url).replace(queryParameters: queryParams);
-
-    debugPrint('uri: $uri');
-    final response = await http.get(
-      uri,
-      headers: <String, String>{
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token',
-      },
-    );
-    // debugPrint('response: ${jsonDecode(utf8.decode(response.bodyBytes))} ${response.statusCode}');
-
-    if (response.statusCode == 200) {
+    try {
+    final response = await dio.get(
+      url, queryParameters: queryParams);
       return response;
-    } else {
-      throw Exception('버스 데이터 가져오기에 실패했습니다.');
+    } catch (e) {
+      throw Exception('버스 데이터를 불러오지 못했습니다.');
     }
   }
 }
