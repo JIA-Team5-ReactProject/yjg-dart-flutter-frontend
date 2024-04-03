f3094c9efcdb15fe29a61f84625d3644c0429b08
gardenkim <syzygy@g.yju.ac.kr>
2024-04-03 17:15:24 +0900

    Refactor: Auth関連APIロジックにDioインターセプターを適用

--- lib/auth/data/data_resources/logout_data_source.dart
+++ lib/auth/data/data_resources/logout_data_source.dart
@@ -1,40 +1,38 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:http/http.dart' as http;
 import 'package:yjg/shared/constants/api_url.dart';
+import 'package:yjg/shared/service/interceptor.dart';
 
 class LogoutDataSource {
-  String getApiUrl() {
-    // 상수 파일에서 가져온 apiURL 사용
-    return apiURL;
-  }
-
+  static final Dio dio = Dio();
   static final storage = FlutterSecureStorage(); // 토큰 담는 곳
+
+  LogoutDataSource() {
+    dio.interceptors.add(DioInterceptor(dio));
+  }
 
   // 로그아웃
   Future<void> postLogoutAPI(WidgetRef ref) async {
-    bool? isAdmin = await storage.read(key: 'isAdmin') == 'ture' ? true : false; // false: Student, true: Admin
+    bool? isAdmin = await storage.read(key: 'isAdmin') == 'ture'
+        ? true
+        : false; // false: Student, true: Admin
     debugPrint('isAdmin: $isAdmin');
-    final token = await storage.read(key: 'auth_token');
     final String url;
     isAdmin == true
         ? url = '$apiURL/api/admin/logout'
         : url = '$apiURL/api/user/logout';
 
-    final response = await http.post(
-      Uri.parse(url),
-      headers: <String, String>{
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token',
-      },
-    );
+    try {
+      await dio.post(url);
 
-    if (response.statusCode == 200) {
       await storage.delete(key: 'auth_token');
       await storage.delete(key: 'refresh_token');
       await storage.delete(key: 'studentName');
       debugPrint('로그아웃 완료');
+    } catch (e) {
+      debugPrint('로그아웃 실패: $e');
     }
   }
 }
