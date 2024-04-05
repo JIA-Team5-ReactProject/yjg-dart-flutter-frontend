902d127ede6823da075d1810b889e6ec52b1f25c
gardenkim <syzygy@g.yju.ac.kr>
2024-04-05 10:14:14 +0900

    Refactor: 外泊/外出申請キャンセルAPIロジックファイルの分離およびdioインターセプターの適用

--- lib/administration/data/data_sources/sleepover_data_source.dart
+++ lib/administration/data/data_sources/sleepover_data_source.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:intl/intl.dart';
 import 'package:yjg/shared/constants/api_url.dart';
@@ -34,9 +35,22 @@ class SleepoverDataSource {
 
     try {
       final response = await dio.get(url);
+      debugPrint('외박/외출 불러오기 완료: ${response.statusCode}, ${response.data}');
       return response;
     } catch (e) {
       throw Exception('예약 불러오기를 실패하였습니다.');
+    }
+  }
+
+  // * 외박/외출 신청 취소하는 DELETE API 함수
+  Future<void> deleteApplication(int id) async {
+    final url = '$apiURL/api/absence/$id';
+
+    try {
+    final response = await dio.delete(url);
+    debugPrint('예약 취소 완료: ${response.statusCode}');
+    } catch (e) {
+      throw Exception('예약 취소를 실패하였습니다.');
     }
   }
 }
--- lib/administration/presentaion/widgets/sleepover_widget.dart
+++ lib/administration/presentaion/widgets/sleepover_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:http/http.dart' as http;
-import 'package:yjg/shared/constants/api_url.dart';
+import 'package:yjg/administration/data/data_sources/sleepover_data_source.dart';
+import 'package:yjg/shared/theme/palette.dart';
 
 class SleepoverWidget extends StatefulWidget {
   final int id;
@@ -24,7 +23,7 @@ class SleepoverWidget extends StatefulWidget {
 }
 
 class _SleepoverWidgetState extends State<SleepoverWidget> {
-  static final storage = FlutterSecureStorage(); //정원이가 말해준 코드(토큰)
+  final _sleepoverDataSource = SleepoverDataSource();
 
   @override
   Widget build(BuildContext context) {
@@ -36,83 +35,32 @@ class _SleepoverWidgetState extends State<SleepoverWidget> {
     switch (widget.apply) {
       case 0:
         statusText = "거절";
-        statusColor = Colors.red;
+        statusColor = Palette.stateColor3;
         break;
       default:
         statusText = "승인";
-        statusColor = Colors.blue;
+        statusColor = Palette.mainColor;
     }
 
-    return Container(
-      width: screenWidth * 0.85,
-      height: 100,
-      padding: EdgeInsets.all(20), // 패딩 추가
-      margin: EdgeInsets.symmetric(vertical: 10), // 수직 마진 추가
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10), // 모서리 둥글게
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 2,
-            blurRadius: 7,
-            offset: Offset(0, 3), // 그림자 효과
-          ),
-        ],
-      ),
+    return Material(
       child: InkWell(
-        child: Container(
-            child: Row(
-          children: [
-            Column(
-              children: [
-                Icon(
-                  Icons.bedtime,
-                  color: statusColor,
-                  size: 40,
-                ),
-                Text(
-                  statusText,
-                  style: TextStyle(color: statusColor, fontSize: 14),
-                ),
-              ],
-            ),
-            SizedBox(
-              width: 20,
-            ),
-            Text(
-              widget.startDate,
-              style: TextStyle(fontSize: 20),
-            ),
-            Text(
-              ' ~ ',
-              style: TextStyle(fontSize: 20),
-            ),
-            Text(
-              widget.lastDate,
-              style: TextStyle(fontSize: 20),
-            ),
-          ],
-        )),
+        splashColor: Colors.grey.withOpacity(0.0),
+        highlightColor: Colors.grey.withOpacity(0.0),
         onTap: () {
           showDialog(
             context: context,
             builder: (context) => AlertDialog(
               backgroundColor: Colors.white,
               title: Container(
-                 padding: EdgeInsets.only(bottom: 10),
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(
-                            color: Color.fromARGB(255, 162, 162, 162),
-                            width: 2, // 여기에서 테두리의 두께를 설정합니다.
-                          ),
-                        ),
-                      ),
+                padding: EdgeInsets.only(bottom: 10),
                 child: Text(
                   widget.apply == 0 ? '거절' : '승인',
                   style: TextStyle(
-                      color: widget.apply == 0 ? Colors.red : Colors.blue),
+                      color: widget.apply == 0
+                          ? Palette.stateColor3
+                          : Palette.mainColor,
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold),
                 ),
               ),
               content: SingleChildScrollView(
@@ -120,22 +68,19 @@ class _SleepoverWidgetState extends State<SleepoverWidget> {
                   children: <Widget>[
                     Container(
                       padding: EdgeInsets.only(bottom: 10),
-                      margin: EdgeInsets.only(bottom: 10,),
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(
-                            color: Color.fromARGB(255, 162, 162, 162),
-                            width: 2, // 여기에서 테두리의 두께를 설정합니다.
-                          ),
-                        ),
+                      margin: EdgeInsets.only(
+                        bottom: 10,
                       ),
-                      child: Text(' ${widget.content}',style: TextStyle(fontSize: 20),),
+                      child: Text(
+                        widget.content,
+                        style: TextStyle(fontSize: 15),
+                      ),
                     ),
                     Text(
-                      '   ${widget.startDate} ~ ${widget.lastDate}',
+                      '신청일자  ${widget.startDate} ~ ${widget.lastDate}',
                       style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 15,
+                        color: Palette.textColor.withOpacity(0.5),
+                        fontSize: 12,
                       ),
                     ),
                   ],
@@ -143,9 +88,9 @@ class _SleepoverWidgetState extends State<SleepoverWidget> {
               ),
               actions: <Widget>[
                 TextButton(
-                  child: Text('신청 취소'),
+                  child: Text('신청 취소', style: TextStyle(color: Palette.stateColor3, fontSize: 13.0)),
                   onPressed: () async {
-                    await deleteApplication(widget.id);
+                    await _sleepoverDataSource.deleteApplication(widget.id);
                     // 결과를 전달하며 Navigator.pop 메서드 호출
                     Navigator.of(context).pop(); //현재 alert창 닫기
                     Navigator.of(context).pop(); //외박/외출 메인 창 닫기
@@ -153,7 +98,7 @@ class _SleepoverWidgetState extends State<SleepoverWidget> {
                   },
                 ),
                 TextButton(
-                  child: Text('닫기'),
+                  child: Text('닫기', style: TextStyle(color: Palette.stateColor4, fontSize: 13.0)),
                   onPressed: () {
                     Navigator.of(context).pop(); // alert 창 닫기
                   },
@@ -162,24 +107,62 @@ class _SleepoverWidgetState extends State<SleepoverWidget> {
             ),
           );
         },
+        child: Container(
+          width: screenWidth * 0.85,
+          height: 100,
+          padding: EdgeInsets.all(20), // 패딩 추가
+          margin: EdgeInsets.symmetric(vertical: 10), // 수직 마진 추가
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10), // 모서리 둥글게
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 1,
+                blurRadius: 8,
+                offset: Offset(0, 2), // 그림자 효과
+              ),
+            ],
+          ),
+          child: InkWell(
+            child: SizedBox(
+                child: Row(
+              children: [
+                Column(
+                  children: [
+                    Icon(
+                      Icons.bedtime,
+                      color: statusColor,
+                      size: 40,
+                    ),
+                    Text(
+                      statusText,
+                      style: TextStyle(color: statusColor, fontSize: 14),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                Text(
+                  widget.startDate,
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                Text(
+                  ' ~ ',
+                  style: TextStyle(fontSize: 15),
+                ),
+                Text(
+                  widget.lastDate,
+                  style: TextStyle(fontSize: 15),
+                ),
+              ],
+            )),
+          ),
+        ),
       ),
     );
-  }
-
-  //삭제 API 함수
-  Future<void> deleteApplication(int id) async {
-    final token = await storage.read(key: 'auth_token'); //정원이가 말해준 코드(토큰 불러오기)
-    final Uri apiUri = Uri.parse('$apiURL/api/absence/$id');
-
-    final response = await http.delete(apiUri, headers: {
-      'Authorization': 'Bearer $token',
-      'Content-Type': 'application/json',
-    });
-
-    if (response.statusCode == 200) {
-      // 성공적으로 삭제됐을 때의 처리 로직 (예: 리스트 새로고침, 성공 메시지 표시 등)
-    } else {
-      // 실패했을 때의 처리 로직 (예: 에러 메시지 표시 등)
-    }
   }
 }
