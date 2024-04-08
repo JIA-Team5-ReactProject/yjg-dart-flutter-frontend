b3a6791ac9d882160c0532de6207eb949a03a0ff
gardenkim <syzygy@g.yju.ac.kr>
2024-04-09 00:15:01 +0900

    Feat: トークン中央管理シングルトンクラスの実装

--- lib/shared/service/interceptor.dart
+++ lib/shared/service/interceptor.dart
@@ -1,12 +1,17 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:yjg/auth/data/data_resources/login_data_source.dart';
+import 'package:yjg/shared/service/token_refresh_manager.dart';
 
 final FlutterSecureStorage storage = FlutterSecureStorage();
 
 // 토큰 로컬 스토리지에서 가져옴
 Future<String?> getToken() async {
   return await storage.read(key: 'auth_token');
+}
+
+// 리프레시 토큰 로컬 스토리지에서 가져옴
+Future<String?> getRefreshToken() async {
+  return await storage.read(key: 'refresh_token');
 }
 
 // 요청, 응답, 에러에 대한 인터셉터
@@ -29,8 +34,14 @@ class DioInterceptor extends Interceptor {
       options.headers["Content-Type"] = "application/json";
     }
 
-    // 인증 토큰이 필요한 경우 헤더에 추가
-    if (options.extra["noAuth"] != true) {
+    // 리프레시 토큰 사용이 필요한 경우
+    if (options.extra["useRefreshToken"] == true) {
+      final refreshToken = await getRefreshToken();
+      if (refreshToken != null) {
+        options.headers["Authorization"] = "Bearer $refreshToken";
+      }
+    } else if (options.extra["noAuth"] != true) {
+      // 일반적인 액세스 토큰 사용 경우
       final token = await getToken();
       if (token != null) {
         options.headers["Authorization"] = "Bearer $token";
@@ -48,26 +59,32 @@ class DioInterceptor extends Interceptor {
   }
 
   @override
+  // 에러 발생 시 실행되는 메서드
   void onError(DioException err, ErrorInterceptorHandler handler) async {
-
-    // 401 Unauthorized 에러가 발생했을 경우, noAuth 옵션이 false인 경우만 처리
+    // 401 Unauthorized 에러 처리
     if (err.response?.statusCode == 401 &&
         err.requestOptions.extra["noAuth"] != true) {
-      // 새 토큰을 얻기 위해 getRefreshTokenAPI 호출
-      await LoginDataSource().getRefreshTokenAPI();
-      // 갱신된 토큰을 사용하여 요청 재시도
-      final token = await getToken(); // 갱신된 토큰 가져오기
-      handler.resolve(await _retry(err.requestOptions, token));
+      try {
+        // TokenRefreshManager를 사용하여 토큰 리프레시 로직 실행
+        await TokenRefreshManager()
+            .refreshTokenIfNeeded(err.requestOptions, dio);
+        // 토큰이 갱신된 후 요청 재시도
+        final response = await _retry(err.requestOptions);
+        handler.resolve(response);
+      } catch (e) {
+        // 재시도에 실패하거나 리프레시 토큰 자체가 실패한 경우
+        super.onError(err, handler);
+      }
     } else {
-      // noAuth가 true이거나 다른 에러인 경우 기존 에러 처리 로직을 그대로 진행
-      handler.next(err);
+      super.onError(err, handler);
     }
   }
 
   // 새 토큰을 사용하여 요청 재시도
-  Future<Response<dynamic>> _retry(
-      RequestOptions requestOptions, String? token) async {
+  Future<Response<dynamic>> _retry(RequestOptions requestOptions) async {
     const maxRetryCount = 3; // 최대 재시도 횟수
+    final token = await getToken(); // 새 토큰 가져오기
+
     requestOptions.headers["Authorization"] = "Bearer $token";
 
     // 재시도 횟수를 가져와서 1 증가
