e19fa705e729f2d07b2f6cdfa5d3b656a3f67198
gardenkim <syzygy@g.yju.ac.kr>
2024-05-07 11:04:11 +0900

    Remove: 不要なロジックの削除

--- lib/auth/domain/usecases/detail_usecase.dart
+++ lib/auth/domain/usecases/detail_usecase.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:yjg/auth/data/data_resources/user_data_source.dart';
 import 'package:yjg/auth/presentation/viewmodels/user_viewmodel.dart';
 import 'package:yjg/shared/theme/palette.dart';
@@ -18,45 +17,35 @@ class DetailUseCase {
           change: change,
         );
 
-    // SecureStorage에서 토큰 가져오기
-    final storage = FlutterSecureStorage();
-    String? token = await storage.read(key: 'auth_token');
-
-    if (token == null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('로그인 정보가 유효하지 않습니다. 다시 로그인해 주세요.'),
-          backgroundColor: Colors.red,
-        ),
-      );
-      return;
-    }
-
     // 추가 정보 입력 API 호출
     try {
       final dataSource = UserDataSource(); // DataSource 인스턴스 생성
       final response = await dataSource.patchAdditionalInfoAPI(ref, change);
 
-      if (response.statusCode == 200) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-              content: Text('회원가입에 성공하였습니다.'),
-              backgroundColor: Palette.mainColor),
-        );
-        // 성공 시 메인 페이지로 이동(이전 페이지로 못 가게 막아버림)
-        Navigator.pushNamedAndRemoveUntil(
-            context, '/dashboard_main', (Route<dynamic> route) => false);
-      } else {
-        throw Exception('서버 에러: ${response.statusCode}');
+      if (context.mounted) {
+        if (response.statusCode == 200) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+                content: Text('회원가입에 성공하였습니다. 다시 로그인해 주세요.'),
+                backgroundColor: Palette.mainColor),
+          );
+          // 성공 시 메인 페이지로 이동(이전 페이지로 못 가게 막아버림)
+          Navigator.pushNamedAndRemoveUntil(
+              context, '/login_student', (Route<dynamic> route) => false);
+        } else {
+          throw Exception('서버 에러: ${response.statusCode}');
+        }
       }
     } catch (e) {
-      // 추가정보 입력 시 에러 메시지 표시
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('추가 정보 입력 실패. 다시 시도해 주세요. 에러: $e'),
-          backgroundColor: Colors.red,
-        ),
-      );
+      debugPrint('추가 정보 입력에 실패했습니다. : $e');
+        // 추가정보 입력 시 에러 메시지 표시
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('추가 정보 입력 실패. 다시 시도해 주세요. 에러: $e'),
+            backgroundColor: Colors.red,
+          ),
+        );
+      
     }
   }
 }
